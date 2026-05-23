61bb4af5130d8659b09acbc2fe52df8062cfd97e
Ojingwa Daniel <ojingwadanny@gmail.com>
2026-05-23 17:26:23 +0100

    added authcontroller for login method

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request){
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        $user = User::where("email",$request->email)->first();
+        if(!$user || !Hash::check($request->password,$user->password)){
+            throw ValidationException::withMessages([
+                "email" =>  "The credential you inputed is not found"
+            ]);
+
+        };
+        $token = $user->createToken("api_token")->plainTextToken;
+        return response()->json([
+            "token" => $token 
+        ]);
+
+
+    }
+
+    public function logout(){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource("events",EventController::class);
-Route::apiResource("events.attendees",AttendeeController::class)->scoped()->except("update");   
+Route::apiResource("events.attendees",
+AttendeeController::class)->scoped()->except("update");  
+Route::post("/login",[AuthController::class,"login"]);
